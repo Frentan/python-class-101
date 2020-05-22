6469e897819f9d909cf6f175bac74171d6fef326
Tulio Geniole <62348349+Frentan@users.noreply.github.com>
2020-05-22 18:06:41 -0300

    Create python-class-101.py

---
+++ python-class-101.py
@@ -0,0 +1,19 @@
+# Starting out with a simple addition program:
+num1 = 2
+num2 = 1
+sum = num1 + num2
+print(sum)
+# Use print() to insert empty lines and keep your output orderly:
+print()
+# Unlike JavaScript and C, it's not mandatory to use semicolons at the end of statements.
+# Neither are curly braces "{}" used to represent blocks. Everything is done with indentation:
+    #I_am_a_parent
+    #    I_am_a_child
+    #        I_am_a_grandchild
+    #    I_am_another_child
+    #        I_am_another_grandchild
+
+print()
+# And finally, a neat easter egg to inspire your journey:
+import this
+
