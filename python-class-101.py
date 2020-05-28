924711f4a13ed013b3292f5b3ed834be6d7dc0bc
Tulio Geniole <62348349+Frentan@users.noreply.github.com>
2020-05-28 17:51:20 -0300

    Update

--- python-class-101.py
+++ python-class-101.py
@@ -3,10 +3,10 @@ num1 = 2
 num2 = 1
 sum = num1 + num2
 print(sum)
-# Use print() to insert empty lines and keep your output orderly:
+# Use *print()* to insert empty lines and keep your output orderly:
 print()
 # Unlike JavaScript and C, it's not mandatory to use semicolons at the end of statements.
-# Neither are curly braces "{}" used to represent blocks. Everything is done with indentation:
+# Neither are {} curly brackets used to represent blocks. Everything is done with indentation:
 """
 I_am_a_parent
     I_am_a_child
@@ -22,7 +22,21 @@ message = "Python is my favorite programming language :-)"
 print(message)
 # Variable names can only have letters, numbers, and underscores, but they cannot start with a number or be the same as reserved keywords.
 print()
-# CONTINUE WITH STRINGS (PAGE 59)
+# Python has a built-in string class. They are bound by either single or double quotation marks.
+# Strings in Python are immutable, or unchangeable, once created (just like in Java).
+# String characters can be accessed using standard [] square bracket syntax and zero-based numbering.
+str1 = "hi"
+print(str1[0])
+print(str1[1])
+print(len(str1))
+print(str1 + " there")
+print()
+# However, unlike in Java, the + operator does not change numbers or other data types to strings. It's necessary to use the *str()* function instead:
+pi = 3.141592
+str2 = "The value of pi is " + str(pi)
+print(str2)
+print()
+
 print()
 # And finally, a neat Easter egg to inspire your journey:
 import this
